a2ff003678ec6c2df57f7a0979ae64ec2c44c836
Loïc Sciampagna <lsciampagna@agilepartner.net>
2023-10-23 16:31:21 +0200

    creation of header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './styles/main.scss';
 
-console.log('ici');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+ 
+  const header = document.querySelector('.tp-header');
+  const triggerNavMobile = document.querySelector('#triggerNavMobile');
+  const navMobile = document.querySelector('.tp-header__row-2');
+
+  triggerNavMobile.addEventListener('click', () => {
+    navMobile.classList.toggle('nav-open');
+  });
+
+  window.addEventListener('scroll', () => {
+    if(window.scrollY > 0) {
+      header.classList.add('tp-header--scroll');
+    }
+    else {
+      header.classList.remove('tp-header--scroll');
+    }
+  })
+});
