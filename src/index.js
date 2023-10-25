52d7ab3d2d6b44c261f37b462cf8213c31fc4c36
Loïc Sciampagna <lsciampagna@agilepartner.net>
2023-10-25 15:22:44 +0200

    work on hp

--- src/index.js
+++ src/index.js
@@ -3,7 +3,25 @@ import './styles/main.scss';
 import './components/header';
 import './components/footer';
 
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+import 'swiper/css';
 
 document.addEventListener('DOMContentLoaded', () => {
-
+  // init Swiper:
+  const swiper = new Swiper('.tp-swiper', {
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 2.5,
+    modules: [Navigation, Pagination],
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 });
